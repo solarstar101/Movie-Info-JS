c5ea4796566e4c51212f3133b3ae7cb276b27dc8
solarstar101 <mmerino1@uth.tmc.edu>
2019-12-19 10:17:45 -0600

    api now working and displaying movies decided to remove jquery vanilla js only

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,6 @@
 var searchForm = document.getElementById("searchForm");
 let searchText = document.getElementById("searchText");
+let movieSection = document.getElementById("movies");
 
 searchForm.addEventListener("submit", e => {
   let searchInput = searchText.value;
@@ -10,10 +11,29 @@ searchForm.addEventListener("submit", e => {
 //fetching movies using axios
 function getMovies(searchInput) {
   axios
-    .get("http://api.rest7.com/v1/movie_info?imdb=" + searchInput)
+    .get("http://www.omdbapi.com/?s=" + searchInput + "&apikey=4aaa4f4e")
     .then(res => {
       console.log(res);
+      let movies = res.data.Search;
+      let output = "";
+
+      for (let movie of movies) {
+        output += `
+        <div class="col-md-3">
+          <div class=well text-center"> 
+          <img src="${movie.Poster}">
+          <h5> ${movie.Title} </h5>
+          <a onclick="movieSelected('${movie.imbdID}')" class="btn btn-primary" href="#">Movie Details </a>
+          </div>
+          </div>
+        `;
+      }
+      console.log(output);
+      if (movieSection) {
+        movieSection.innerHTML += output;
+      }
     })
+
     .catch(err => {
       console.log(err);
     });
